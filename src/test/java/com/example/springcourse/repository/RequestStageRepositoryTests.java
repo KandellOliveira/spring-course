1bb56d33008328f13830fc494823fbc3930c56dc
Kandell Oliveira <jiukao@yahoo.com.br>
2019-04-08 22:06:04 -0300

    Ajuste no requestStageRepositoryTests

--- src/test/java/com/example/springcourse/repository/RequestStageRepositoryTests.java
+++ src/test/java/com/example/springcourse/repository/RequestStageRepositoryTests.java
@@ -3,8 +3,13 @@ package com.example.springcourse.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
+import org.junit.FixMethodOrder;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -15,11 +20,13 @@ import com.example.springcourse.domain.User;
 import com.example.springcourse.domain.enums.RequestState;
 
 @RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @SpringBootTest
 public class RequestStageRepositoryTests {
 	
 	@Autowired private RequestStageRepository requestStageRepository;
 
+	@Test
 	public void saveTest() {
 		User user = new User();
 		user.setId(1L);
@@ -33,12 +40,21 @@ public class RequestStageRepositoryTests {
 		
 		assertThat(createdStage.getId()).isEqualTo(1L);
 	}
-	
+
+	@Test
 	public void getByIdTest() {
+
+		Optional<RequestStage> result =  requestStageRepository.findById(1L);
+
+		RequestStage requestStage = result.get();
+		assertThat(requestStage.getDescrition()).isEqualTo("Something");
 		
 	}
-	
+
+	@Test
 	public void listByRequestIdTest() {
-		
+		List<RequestStage> stages = requestStageRepository.findAll();
+
+		assertThat(stages.size()).isEqualTo(1);
 	}
 }
