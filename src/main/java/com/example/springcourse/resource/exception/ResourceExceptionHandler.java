fe264ce1977836f9240af6ae3a9b5cbe12a964f4
Kandell Oliveira <kandell.oliveira@esig.com.br>
2019-08-01 12:49:00 -0300

    Adição de list de erros para o usuário saber quais atributos estão invalidos

--- src/main/java/com/example/springcourse/resource/exception/ResourceExceptionHandler.java
+++ src/main/java/com/example/springcourse/resource/exception/ResourceExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.example.springcourse.resource.exception;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import org.postgresql.shaded.com.ongres.scram.common.message.ServerFinalMessage.Error;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,23 +17,28 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import com.example.springcourse.exception.NotFoundException;
 
 @ControllerAdvice
-public class ResourceExceptionHandler extends ResponseEntityExceptionHandler{
-	
+public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
+
 	@ExceptionHandler(NotFoundException.class)
-	public ResponseEntity<ApiError> handleNotFoundException(NotFoundException ex){
+	public ResponseEntity<ApiError> handleNotFoundException(NotFoundException ex) {
 		ApiError error = new ApiError(HttpStatus.NOT_FOUND.value(), ex.getMessage(), new Date());
-		
+
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
-	
+
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		List<String> errors = new ArrayList<String>();
+		ex.getBindingResult().getAllErrors().forEach(error -> {
+			errors.add(error.getDefaultMessage());
+		});
 		
-		String defaultMessage = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
-		
-		ApiError error = new ApiError(HttpStatus.BAD_REQUEST.value(), defaultMessage, new Date());
-		
+		String defaultMessage = "Invalid field(s)";
+
+		ApiErrorList error = new ApiErrorList(HttpStatus.BAD_REQUEST.value(), defaultMessage, new Date(), errors);
+
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 
